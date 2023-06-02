a8d8abd38217ff0d27165fcba98569a6f54ae591
shmokmt <32533860+shmokmt@users.noreply.github.com>
2023-06-02 21:35:35 +0900

    Use go generate to eliminate direct dependencies on the GitHub API

---
+++ cmd/tf-docs-finder/main.go
@@ -0,0 +1,37 @@
+//go:generate go run ../../gen/main.go
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/ktr0731/go-fuzzyfinder"
+	tfdocsfinder "github.com/shmokmt/tf-docs-finder"
+)
+
+func openBrowser(url string) error {
+	switch runtime.GOOS {
+	case "linux":
+		return exec.Command("xdg-open", url).Start()
+	case "windows":
+		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		return exec.Command("open", url).Start()
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+}
+
+func main() {
+	idx, _ := fuzzyfinder.Find(tfdocsfinder.AwsResources, func(i int) string {
+		return tfdocsfinder.AwsResources[i]
+	})
+	url := fmt.Sprintf("https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/%s", strings.TrimPrefix(tfdocsfinder.AwsResources[idx], "aws_"))
+	if err := openBrowser(url); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/google/go-github/v39 v39.2.0
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
 	golang.org/x/net v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
