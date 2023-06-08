e10c1b22254073baf697f808fb4a43c821d282e6
shmokmt <32533860+shmokmt@users.noreply.github.com>
2023-06-08 22:47:07 +0900

    Supports for data-sources

--- cmds/aws.go
+++ cmds/aws.go
@@ -12,15 +12,32 @@ import (
 var AwsCommand = &cli.Command{
 	Name:        "aws",
 	Description: "Open hashicorp/aws's docs",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "type",
+			Value: "resources",
+			Usage: "resources or data-sources. default is resources.",
+		},
+	},
 	Action: func(cCtx *cli.Context) error {
-		idx, err := fuzzyfinder.Find(tfdocsfinder.AwsResources, func(i int) string {
-			return tfdocsfinder.AwsResources[i]
+		tfType := cCtx.String("type")
+		var list []string
+		switch tfType {
+		case "resources":
+			list = tfdocsfinder.AwsResources
+		case "data-sources":
+			list = tfdocsfinder.AwsDataSources
+		default:
+			return cli.Exit(fmt.Errorf("invalid type: %s", tfType), 1)
+		}
+		idx, err := fuzzyfinder.Find(list, func(i int) string {
+			return list[i]
 		})
 		if err != nil {
 			return nil
 		}
-		fmt.Println(tfdocsfinder.AwsResources[idx])
-		url := fmt.Sprintf("https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/%s", strings.TrimPrefix(tfdocsfinder.AwsResources[idx], "aws_"))
+		fmt.Println(list[idx])
+		url := fmt.Sprintf("https://registry.terraform.io/providers/hashicorp/aws/latest/docs/%s/%s", tfType, strings.TrimPrefix(list[idx], "aws_"))
 		if err := openBrowser(url); err != nil {
 			return cli.Exit(err, 1)
 		}
